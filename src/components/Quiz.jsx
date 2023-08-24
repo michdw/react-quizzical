e27a630dfbd85345e2f011010d18932e9ce4a4c5
Michael D Weaver <michd.weaver@gmail.com>
2023-08-23 22:37:46 -0400

    correct-incorrect answers displaying dynamically

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,35 +1,16 @@
 import React from "react";
+import "./Quiz.css";
 
 export default function Quiz(props) {
   const [quiz, setQuiz] = React.useState();
-  const [complete, setComplete] = React.useState(false);
+  const [active, setActive] = React.useState(false);
   const dataFetchedRef = React.useRef(false);
 
-  const toggleComplete = () => {
-    setComplete(!complete);
-    if (complete) getNewQuiz();
+  const toggleActive = () => {
+    setActive(!active);
+    if (active) getNewQuiz();
+    if (!active) revealAnswers(true);
   };
-
-  const actionButton = () => {
-    let btnText = complete ? "start new quiz" : "show answers";
-    return <button onClick={toggleComplete}>{btnText}</button>;
-  };
-
-  function getNewQuiz() {
-    fetch("https://opentdb.com/api.php?amount=10").then((resp) =>
-      resp
-        .json()
-        .then((data) => {
-          setQuiz(data);
-          showQuizData(data);
-        })
-        .catch((error) => console.log(error))
-    );
-  }
-
-  function showQuizData(data) {
-    console.log(data.results[0].question);
-  }
 
   React.useEffect(() => {
     if (dataFetchedRef.current) return;
@@ -37,9 +18,70 @@ export default function Quiz(props) {
     getNewQuiz();
   }, []);
 
+  function getNewQuiz() {
+    fetch("https://opentdb.com/api.php?amount=10").then((resp) =>
+      resp
+        .json()
+        .then((data) => {
+          setQuiz(data);
+          console.log(data);
+        })
+        .catch((error) => console.log(error))
+    );
+    revealAnswers(false)
+  }
+
+  function revealAnswers(show) {
+    const correctAnswers = document.querySelectorAll('[data-correct="true"]');
+    correctAnswers.forEach((element) => {
+      show ? element.classList.remove("unrevealed-option") : element.classList.add("unrevealed-option")
+      show ? element.classList.add("correct-answer") : element.classList.remove("correct-answer")
+    });
+    const incorrectAnswers = document.querySelectorAll('[data-correct="false"]');
+    incorrectAnswers.forEach((element) => {
+      show ? element.classList.remove("unrevealed-option") : element.classList.add("unrevealed-option")
+      show ? element.classList.add("incorrect-answer") : element.classList.remove("incorrect-answer")
+    });
+  }
+
+  function getOptions(result) {
+    let allOptions = [...result.incorrect_answers]
+      .concat(result.correct_answer)
+      .sort();
+    return allOptions.map((option) => {
+      return (
+        <span
+          className="option unrevealed-option"
+          data-correct={option === result.correct_answer ? "true" : "false"}
+        >
+          {option}
+        </span>
+      );
+    });
+  }
+
+  const quizData = dataFetchedRef.current ? (
+    quiz.results.map((result) => (
+      <div>
+        <p className="question">{result.question}</p>
+        <div>
+          <div>{getOptions(result)}</div>
+        </div>
+      </div>
+    ))
+  ) : (
+    <div>loading</div>
+  );
+
+  const actionButton = () => {
+    let btnText = active ? "start new quiz" : "show answers";
+    return <button onClick={toggleActive}>{btnText}</button>;
+  };
+
   return (
     <div className="quizPage">
       <h1>quiz page</h1>
+      {quizData}
       {actionButton()}
       <button onClick={props.loadStartPage}>Home</button>
     </div>
