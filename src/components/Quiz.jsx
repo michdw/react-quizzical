022536785ad23351c89c3af5e24c09b416b50dc5
Michael D Weaver <michd.weaver@gmail.com>
2023-08-26 14:21:35 -0400

    radio options working, selecting by clicking on label

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,23 +1,23 @@
 import React from "react";
 import "./Quiz.css";
-import {decode} from "html-entities"
+import { decode } from "html-entities";
 
 export default function Quiz(props) {
   const [quiz, setQuiz] = React.useState();
   const [active, setActive] = React.useState(false);
   const dataFetchedRef = React.useRef(false);
 
-  const toggleActive = () => {
-    setActive(!active);
-    if (active) getNewQuiz();
-    if (!active) revealAnswers(true);
-  };
-
   React.useEffect(() => {
     if (dataFetchedRef.current) return;
     dataFetchedRef.current = true;
     getNewQuiz();
-  }, []);
+  });
+
+  function toggleActive() {
+    setActive(!active);
+    if (active) getNewQuiz();
+    if (!active) toggleShowAnswers(true);
+  }
 
   function getNewQuiz() {
     fetch("https://opentdb.com/api.php?amount=10").then((resp) =>
@@ -25,49 +25,69 @@ export default function Quiz(props) {
         .json()
         .then((data) => {
           setQuiz(data);
-          console.log(data)
+          console.log(data);
         })
         .catch((error) => console.log(error))
     );
-    revealAnswers(false)
+    toggleShowAnswers(false);
   }
 
-  function revealAnswers(show) {
+  function toggleShowAnswers(show) {
     const correctAnswers = document.querySelectorAll('[data-correct="true"]');
     correctAnswers.forEach((element) => {
-      show ? element.classList.remove("unrevealed-option") : element.classList.add("unrevealed-option")
-      show ? element.classList.add("correct-answer") : element.classList.remove("correct-answer")
+      show
+        ? element.classList.remove("unrevealed-option")
+        : element.classList.add("unrevealed-option");
+      show
+        ? element.classList.add("correct-answer")
+        : element.classList.remove("correct-answer");
     });
-    const incorrectAnswers = document.querySelectorAll('[data-correct="false"]');
+    const incorrectAnswers = document.querySelectorAll(
+      '[data-correct="false"]'
+    );
     incorrectAnswers.forEach((element) => {
-      show ? element.classList.remove("unrevealed-option") : element.classList.add("unrevealed-option")
-      show ? element.classList.add("incorrect-answer") : element.classList.remove("incorrect-answer")
+      show
+        ? element.classList.remove("unrevealed-option")
+        : element.classList.add("unrevealed-option");
+      show
+        ? element.classList.add("incorrect-answer")
+        : element.classList.remove("incorrect-answer");
     });
   }
 
-  function getOptions(result) {
+  function getOptions(result, questionIndex) {
     let allOptions = [...result.incorrect_answers]
       .concat(result.correct_answer)
       .sort();
     return allOptions.map((option, index) => {
-      option = decode(option)
+      let optionIndex = `${questionIndex}.${index}`;
+      option = decode(option);
       return (
-        <span key={index}
-          className="option unrevealed-option"
-          data-correct={option === result.correct_answer ? "true" : "false"}
-        >
-          {option}
+        <span key={index} className="option unrevealed-option">
+          <label htmlFor={optionIndex} onClick={showChoice}>
+            <input
+              type="radio"
+              name={questionIndex}
+              id={optionIndex}
+              value={optionIndex}
+            />
+            <i>{option}</i>
+          </label>
         </span>
       );
     });
   }
+
+  const showChoice = ((event) => {
+    
+  })
 
   const quizData = dataFetchedRef.current ? (
     quiz.results.map((result, index) => (
       <div key={index}>
         <p className="question">{decode(result.question)}</p>
         <div>
-          <div>{getOptions(result)}</div>
+          <div>{getOptions(result, index)}</div>
         </div>
       </div>
     ))
@@ -77,15 +97,21 @@ export default function Quiz(props) {
 
   const actionButton = () => {
     let btnText = active ? "start new quiz" : "show answers";
-    return <button disabled={!dataFetchedRef.current} onClick={toggleActive}>{btnText}</button>;
+    return (
+      <button disabled={!dataFetchedRef.current} onClick={toggleActive}>
+        {btnText}
+      </button>
+    );
   };
 
   return (
     <div className="quizPage">
       <h1>quiz page</h1>
       {quizData}
-      {actionButton()}
-      <button onClick={props.loadStartPage}>Home</button>
+      <div className="navigation">
+        {actionButton()}
+        <button onClick={props.loadStartPage}>Home</button>
+      </div>
     </div>
   );
 }
