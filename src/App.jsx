7e0a95ea5a658e3bd2cd88c831eb8ec633330a8b
Michael D Weaver <michd.weaver@gmail.com>
2023-09-07 15:15:01 -0400

    renamed Shape component, dynamic styling working

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import "./App.css";
 import React from "react";
 import Start from "./components/Start";
 import Quiz from "./components/Quiz";
-import Blob from "./components/Blob";
+import Shape from "./components/Shape";
 
 export default function App() {
   const [page, setPage] = React.useState("start");
@@ -33,11 +33,18 @@ export default function App() {
     <Quiz quizLength={quizLength} loadStartPage={() => switchPage("start")} />
   );
 
+  const shapes = () => {
+    let shapes = []
+    for (let i = 0; i < 2; i++) {
+      shapes.push(<Shape key={i} index={i}></Shape>)
+    }
+    return shapes;
+  }
+
   return (
     <div className="App">
       <div className="bgLayer">
-        <Blob fill="#FFFAD1" index={1}></Blob>
-        <Blob fill="#DEEBF8" index={2}></Blob>
+        {shapes()}
       </div>
       {getPage()}
     </div>
--- src/components/Shape.jsx
+++ src/components/Shape.jsx
@@ -1,12 +1,19 @@
 import React from "react";
 
-export default function Blob(props) {
-  const transform = props.index === 1 ? "translateX(100px) rotate(90deg)" : ""
+export default function shape(props) {
+  const styles = [
+    { transform: "translateX(100px)", top: "100px" },
+    { transform: "translateY(100px)", top: "200px" },
+  ];
+  const fillColors = [
+    "#FFFAD1",
+    "#DEEBF8"
+  ]
 
   return (
     <svg
-      style={{ transform: transform }}
-      className="blob"
+      style={{ ...styles[props.index] }}
+      className="shape"
       width="201"
       height="142"
       viewBox="0 0 201 142"
@@ -17,7 +24,7 @@ export default function Blob(props) {
         fillRule="evenodd"
         clipRule="evenodd"
         d="M97.081 2.96445C125.176 1.07254 156.497 -5.87426 176.843 11.5469C199.363 30.8293 205.928 62.0589 197.414 88.8747C189.338 114.313 164.282 132.992 135.909 140.475C112.024 146.775 90.3937 132.093 67.6086 123.11C43.0847 113.441 10.3944 110.575 2.2058 87.7612C-6.33319 63.9714 11.3227 39.0277 31.0878 21.3621C48.2313 6.03978 72.9064 4.59238 97.081 2.96445Z"
-        fill={props.fill}
+        fill={fillColors[props.index]}
       />
     </svg>
   );
